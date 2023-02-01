8cb80bb464ca47e1ecc007e8eeb3a9f44f292277
Ligandro Singh Yumnam <97714265+ligandro@users.noreply.github.com>
2023-02-02 00:19:27 +0530

    first commit

---
+++ predict_page.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+regressor = data["model"]
+le_country = data["le_country"]
+le_education = data["le_education"]
+
+def show_predict_page():
+    st.title(":computer: Software Developer Salary Prediction :money_with_wings:")
+
+    st.write("""### We need some information to predict the salary""")
+
+    countries = (
+        "United States of America",
+        "India",
+        "United Kingdom of Great Britain and Northern Ireland",
+        "Germany",
+        "Canada",
+        "Brazil",
+        "France",
+        "Spain",
+        "Australia",
+        "Netherlands",
+        "Poland",
+        "Italy",
+        "Russian Federation",
+        "Sweden","Turkey" ,"Switzerland","Israel","Norway","Mexico","Ukraine","Denmark","Belgium""South Africa"
+    )
+
+    education = (
+        "Less than a Bachelors",
+        "Bachelor’s degree",
+        "Master’s degree",
+        "Post grad",
+    )
+
+    country = st.selectbox(":earth_asia: Country", countries)
+    education = st.selectbox(":mortar_board: Education Level", education)
+
+    expericence = st.slider(":older_man: Years of Experience", 0, 50, 1)
+
+    ok = st.button("Calculate Salary")
+    if ok:
+        X = np.array([[country, education, expericence ]])
+        X[:, 0] = le_country.transform(X[:,0])
+        X[:, 1] = le_education.transform(X[:,1])
+        X = X.astype(float)
+
+        salary = regressor.predict(X)
+        st.subheader(f"The estimated yearly salary is ${salary[0]:.2f}")
